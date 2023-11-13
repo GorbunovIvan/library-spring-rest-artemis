2357feaac69176af361dcb342d8d344c9f95376d
GorbunovIvan <gorbunovvivan@gmail.com>
2023-11-13 09:27:57 +0200

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+Look at 'library + MQ.png'
---
+++ src/main/java/com/example/BooksSpringRestArtemisApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksSpringRestArtemisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooksSpringRestArtemisApplication.class, args);
+    }
+
+}
