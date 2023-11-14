a2c016c1524a77149fb936966279cca8743d9585
GorbunovIvan <gorbunovvivan@gmail.com>
2023-11-14 15:20:17 +0200

    apache artemis activeMQ is added

--- src/main/java/com/example/controller/EventRecordController.java
+++ src/main/java/com/example/controller/EventRecordController.java
@@ -3,7 +3,9 @@ package com.example.controller;
 import com.example.model.EventRecord;
 import com.example.service.EventRecordService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -12,9 +14,12 @@ import java.util.Set;
 @RestController
 @RequestMapping("/api/v1/events")
 @RequiredArgsConstructor
+@Slf4j
 public class EventRecordController {
 
     private final EventRecordService eventRecordService;
+
+    private final JmsTemplate jmsTemplate;
 
     @GetMapping("/{id}")
     public ResponseEntity<EventRecord> getById(@PathVariable Long id) {
@@ -36,5 +41,13 @@ public class EventRecordController {
                                                                     @RequestParam("timeTo") LocalDateTime timeTo) {
         var events = eventRecordService.getAllByDateTimeBetween(timeFrom, timeTo);
         return ResponseEntity.ok(events);
+    }
+
+    @PostMapping("/{queue}")
+    public ResponseEntity<?> addEventToQueue(@PathVariable String queue,
+                                                @RequestBody String message) {
+        jmsTemplate.convertAndSend(queue, message);
+        log.info("Message was added to queue");
+        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/com/example/service/EventRecordService.java
+++ src/main/java/com/example/service/EventRecordService.java
@@ -1,18 +1,29 @@
 package com.example.service;
 
+import com.example.model.Book;
 import com.example.model.EventRecord;
 import com.example.repository.EventRecordRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
 import org.springframework.stereotype.Service;
 
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import java.util.Objects;
 import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class EventRecordService {
 
     private final EventRecordRepository eventRecordRepository;
+    private final ObjectMapper objectMapper;
 
     public EventRecord getById(Long id) {
         return eventRecordRepository.findById(id)
@@ -29,5 +40,44 @@ public class EventRecordService {
 
     public EventRecord create(EventRecord eventRecord) {
         return eventRecordRepository.merge(eventRecord);
+    }
+
+    @JmsListener(destination = "*")
+    public void receiveMessages(Message<String> message) {
+
+        var headers = message.getHeaders();
+
+        var eventRecord = new EventRecord();
+        eventRecord.setMessage(message.getPayload());
+        eventRecord.setSentTime(new Timestamp(Objects.requireNonNullElse(headers.getTimestamp(), 0L)).toLocalDateTime());
+        eventRecord.setReceivedTime(LocalDateTime.now());
+        eventRecord.setQueueName(getQueueNameFromJMSMessageHeaders(headers));
+
+        var messageString = message.getPayload();
+
+        Book book;
+
+        try {
+            book = objectMapper.readValue(messageString, Book.class);
+        } catch (JsonProcessingException e) {
+            log.error("Book from message can not be parsed: {}", messageString);
+            throw new RuntimeException(e);
+        }
+
+        eventRecord.setBook(book);
+
+        create(eventRecord);
+    }
+
+    private String getQueueNameFromJMSMessageHeaders(MessageHeaders headers) {
+        var destination = headers.get("jms_destination");
+        if (destination == null) {
+            return "";
+        }
+        var destinationName = destination.toString();
+        if (destinationName.startsWith("queue://")) {
+            destinationName = destinationName.substring("queue://".length());
+        }
+        return destinationName;
     }
 }
